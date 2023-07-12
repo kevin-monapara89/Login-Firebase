f6b7c5f6fd370ac77d1f5dc2ae44a4106b0ea933
R36 <kevinmonapara89@gmail.com>
2023-07-12 10:46:40 +0530

    commit

--- app/src/main/java/com/example/loginfirebase/MainActivity.kt
+++ app/src/main/java/com/example/loginfirebase/MainActivity.kt
@@ -1,14 +1,41 @@
 package com.example.loginfirebase
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.loginfirebase.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
 
+        binding.forgetpass.setOnClickListener {
+            var email = binding.email.text.toString()
+
+            auth.sendPasswordResetEmail(email)
+        }
+        binding.login.setOnClickListener {
+
+            var email = binding.email.text.toString()
+            var password = binding.password.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.e(TAG, "success")
+                }
+            }.addOnFailureListener{
+                Log.e(TAG, "====== ${it.message}")
+            }
+        }
 
     }
 }
